8998d97c3a03894dc2d8509ae0477728b4113aed
EijiGorilla <emasuz@hotmail.com>
2026-08-16 13:48:19 +0800

    updated

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -145,6 +145,8 @@ const Chart = () => {
     }
 
     const root = rootSetter({ chartID: chartID });
+    root.setThemes([]);
+
     const chart = root.container.children.push(
       am5xy.XYChart.new(root, {
         panX: false,
